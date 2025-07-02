7c33e2ea889eb677f47f7ebc37c45d5efae78c82
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2025-07-02 13:24:47 +0200

    client updated

--- src/client.c
+++ src/client.c
@@ -12,3 +12,40 @@
 
 #include "../include/minitalk.h"
 
+void send_char(pid_t pid, char c)
+{
+    int i;
+
+    i = 7;
+    while( i >= 0)
+    {
+        if((c >> i) & 1)
+            kill(pid, SIGUSR1);
+        else
+            kill(pid,SIGUSR2);
+        usleep(100); // Delay per sicurezza
+        // TODO BONUS: qui potresti aspettare un segnale di ACK dal server
+        i--;
+    }
+
+}
+
+int main(int ac, char **av)
+{
+    if(ac != 3)
+        return(1);
+    
+    pid_t pid;
+    char *msg;
+    int i;
+
+    pid = atoi(av[1]); // DA CAMBIARE DOPO
+    msg = av[2];
+    i = 0;
+    while(msg[i])
+    {
+        send_char(pid, msg[i]);
+        i++;
+    }
+    
+}
