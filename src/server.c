6f9e691d7c912af2ba197c1e0decef3fe1a3b05f
Fabio Vitharana <87120549+FabzHub17@users.noreply.github.com>
2025-07-04 14:12:47 +0200

    updated

--- src/server.c
+++ src/server.c
@@ -13,29 +13,38 @@
 #include "../include/minitalk.h"
 #include <stdio.h>
 
-/*
-    * The server process waits for signals from clients. (SIGUSR1 = 1 and SIGUSR2 = 0)
-    * It prints the PID of the server process to the console.
-    * IT needs to have handlers for the signals to process incoming data.
-    * The server will run indefinitely, waiting for signals.
-    * in computer science, a signal is a message that is sent to a process to indicate 
-    * ->a particular event has occurred or to request a particular action to be taken. 
-    * ->A process is a program that is being executed by the computer.
-*/
-
-void sig_handler(int sig)
+/* void sig_handler(int sig)
 {
     static int bit_count;
     static unsigned char c;
 
     bit_count = 0;
     c = 0;
-
     if(sig == SIGUSR1)
         c |= (1 << bit_count);
+    bit_count++;
+
     if(bit_count == 8)
     {
         write(1,&c,1);  //  Da cambiare poi dopo
+        bit_count = 0;
+        c = 0;
+        // TODO BONUS
+    }
+} */
+
+void sig_handler(int sig)
+{
+    static int bit_count = 0;
+    static unsigned char c = 0;
+
+    if(sig == SIGUSR1)
+        c |= (1 << (7 - bit_count));
+    bit_count++;
+
+    if(bit_count == 8)
+    {
+        write(1,&c,1);
         bit_count = 0;
         c = 0;
         // TODO BONUS
@@ -61,5 +70,6 @@ int main(void)
 
     return(EXIT_SUCCESS);
 }
+
 
 
